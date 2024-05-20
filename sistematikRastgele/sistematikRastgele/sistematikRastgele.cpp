df5159e64cb03413a1e8aba8e9768c7b402f4b9f
OmerF-CEYLAN <omerfceylan2004@gmail.com>
2024-05-20 12:38:21 +0300

    Systematic Random Sampling
    A data set is created based on the Systematic Random Sampling.

---
+++ sistematikRastgele/sistematikRastgele/sistematikRastgele.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <cstdlib> 
+#include <iostream> 
+#include <time.h>
+using namespace std;
+
+int main()
+{
+    
+    double N,n,k;
+
+    cout << "Evren hacmi (N) giriniz : ";
+    cin >> N;
+
+    cout << endl << "Orneklem hacmi (n) giriniz : ";
+    cin >> n;
+   
+    k = N / n;
+
+
+    srand(time(NULL));
+
+    int a = 1 + rand() % ((int)k - 1);
+    cout << endl;
+
+
+    for (int i = 0; i < n; i++)
+    {
+
+        cout << a + (int)k * i << " ";
+
+    }
+
+
+}
